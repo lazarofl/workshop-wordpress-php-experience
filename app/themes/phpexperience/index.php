68c9c9e9f698461bc89dd4ae5e79850aea56a13c
Lazaro Fernandes Lima Suleiman <lazaro.fernandes@lambda3.com.br>
2017-11-07 22:24:04 -0200

    primeiro commit

--- app/themes/phpexperience/index.php
+++ app/themes/phpexperience/index.php
@@ -1,7 +1,8 @@
 <?php get_header(); ?>
 <div id="content">
         <section class="intro">
-            <h1>Olá, <span class="nl"></span> estamos no ar</h1>
+            <h1>Olá pessoal, <span class="nl"></span> estamos no ar</h1>
+            <h2>Meetup Delivering Software PHP é vida!</h2>
             <p>
                 Esta é a forma que nós encontramos de realizar entrega contínua mais simples possível. Agora evolua e nos mostre a sua forma! 
             </p>
